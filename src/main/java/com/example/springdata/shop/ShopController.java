bce08f6b1fc70e52bdf29c0c731a6aa1fc49dbd2
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-09-14 19:35:34 +0200

    Make it a REST API

--- src/main/java/com/example/springdata/shop/ShopController.java
+++ src/main/java/com/example/springdata/shop/ShopController.java
@@ -1,15 +1,20 @@
 package com.example.springdata.shop;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class ShopService {
+@RestController
+@RequestMapping("/shop")
+public class ShopController {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) throws ProductNotAvailableException {
+    @PostMapping("/orders")
+    public Order addOrder(@RequestParam List<String> productIds) throws ProductNotAvailableException {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
             Product productToOrder = productRepo.getProductById(productId)
@@ -22,11 +27,13 @@ public class ShopService {
         return orderRepo.addOrder(newOrder);
     }
 
-    public List<Order> findAllOrders(OrderStatus status) {
+    @GetMapping("/orders")
+    public List<Order> findAllOrders(@RequestParam OrderStatus status) {
         return orderRepo.findAllOrders(status);
     }
 
-    public void updateOrder(String orderId, OrderStatus newStatus) {
+    @PutMapping("/orders/{orderId}")
+    public void updateOrder(@PathVariable String orderId, @RequestParam OrderStatus newStatus) {
         Order oldOrder = orderRepo.getOrderById(orderId);
         orderRepo.removeOrder(orderId);
         Order newOrder = oldOrder.withStatus(newStatus);
